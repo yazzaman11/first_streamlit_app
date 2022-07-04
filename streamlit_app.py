a1a49b22d3874cf7597d352bbf50fa6d9f538bfd
yazzaman11 <77803587+yazzaman11@users.noreply.github.com>
2022-07-04 15:58:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 add_my_fruit = streamlit.text_input ('What fruit would you like to add?')
-streamlit.write('Thanks for adding',fruit_choice)
+streamlit.write('Thanks for adding',add_my_fruit)
 
 
 import requests
